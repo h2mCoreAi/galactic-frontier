f4b9b48891e62d53d3c89452d9cac4ab174d5f29
KestrylX <mitchhoward@gmail.com>
2025-10-27 21:06:47 -0400

    fix(backend): define maybeVerifyToken; chore: restore ship-main.sh

--- server.js
+++ server.js
@@ -326,7 +326,6 @@ const maybeVerifyToken = (req, res, next) => {
   if (IS_PRODUCTION) {
     return verifyToken(req, res, next);
   }
-  // Dev: allow unauthenticated access
   return next();
 };
 
